e3bbd83448707047492319584ba124a50b317412
github username <pham.kim.a@gmail.com>
2018-09-12 13:22:39 -0500

    Fix cookies singularizing

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,7 @@ Bundler.require
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
 require_all 'app'
+
+ActiveSupport::Inflector.inflections do |inflect|
+    inflect.irregular "owned_cookie", "owned_cookies"
+end
